bf4e8273e761e91d6b66a0f717ea5cfae8c19d6d
Roger Vogel <roger@puldypartners.com>
2022-12-20 13:23:40 -0800

    First commit

--- Sources/DateManager/DateManager.swift
+++ Sources/DateManager/DateManager.swift
@@ -1,6 +1,283 @@
-public struct DateManager {
-    public private(set) var text = "Hello, World!"
 
-    public init() {
+import UIKit
+
+enum ClockPreference: Int { case c12, c24 }
+enum DateString: Int { case date, dayAndDate, dayDateAndTime }
+
+class DateManager: NSObject {
+    
+    // MARK: - PROPERTIES
+    var theDate: Date?
+    var dateFormatter = DateFormatter()
+    var clockPreference: ClockPreference = .c12
+   
+    let usd = "M/d/YY"
+    let eud = "dd/MM/yyyy"
+    let ust = "h:mm a"
+    let eut = "HH:mm"
+    let usdd = "E MM/dd/yyyy"
+    let eudd = "E dd/MM/yyyy"
+    let usddt = "E MM/dd/yyyy 'at' h:mm a"
+    let euddt = "E dd/MM/yyyy 'at' HH:mm"
+    let api = "yyyy-MM-dd.HH:mm"
+    
+    // MARK: - COMPUTED PROPERTIES
+    var dateValue: UInt64 {
+        
+        get { return UInt64(theDate!.timeIntervalSince1970) }
+    }
+    
+    var gmtDate: Date {
+        
+        get {
+            
+            var GMTDate = Date()
+            
+            let secondsFromGMT = -(TimeZone.current.secondsFromGMT())
+            GMTDate = theDate!.addingTimeInterval(TimeInterval(secondsFromGMT))
+            
+            return GMTDate
+        }
+    }
+  
+    var localDate: Date {
+        
+        get { return theDate! }
+        set { theDate = newValue }
+    }
+  
+    var serverDateAndTime: String {
+        
+        get {
+            
+            dateFormatter.dateFormat = api
+            return dateFormatter.string(from: gmtDate)
+        }
+           
+        set {
+            
+            dateFormatter.dateFormat = api
+            let gmtDate = dateFormatter.date(from: newValue)!
+            
+            let secondsFromGMT = TimeZone.current.secondsFromGMT()
+            theDate = gmtDate.addingTimeInterval(TimeInterval(secondsFromGMT))
+        }
+    }
+    
+    var dateComponents: DateComponents {
+        
+        get {
+            
+            var dateComponents = DateComponents()
+            let calendar = Calendar.current
+            
+            // Convert and store as date components in appointment record
+            dateComponents.year = calendar.component(.year, from: theDate!)
+            dateComponents.month = calendar.component(.month, from: theDate!)
+            dateComponents.day = calendar.component(.day, from: theDate!)
+            dateComponents.hour = calendar.component(.hour, from: theDate!)
+            dateComponents.minute = calendar.component(.minute, from: theDate!)
+            dateComponents.weekOfYear = calendar.component(.weekOfYear, from: theDate!)
+            
+            return dateComponents
+        }
+        
+        set { theDate = NSCalendar.current.date(from: newValue)! }
+    }
+    
+    var dayDateAndTimeString: String {
+        
+        get {
+            
+            if clockPreference == .c12 { dateFormatter.dateFormat = usddt } else {dateFormatter.dateFormat = euddt }
+            return dateFormatter.string(from: theDate!)
+        }
+        
+        set {
+            
+            if clockPreference == .c12 { dateFormatter.dateFormat = usddt }
+            else { dateFormatter.dateFormat = euddt }
+            
+            theDate = dateFormatter.date(from: newValue)
+        }
+    }
+    
+    var dayAndDateString: String {
+        
+        get {
+            
+            if clockPreference == .c12 { dateFormatter.dateFormat = usdd } else {dateFormatter.dateFormat = eudd }
+            return dateFormatter.string(from: theDate!)
+        }
+        
+        set {
+            
+            if clockPreference == .c12 { dateFormatter.dateFormat = usdd }
+            else { dateFormatter.dateFormat = eudd }
+            
+            theDate = dateFormatter.date(from: newValue)
+        }
+    }
+    
+    var dateString: String {
+        
+        get {
+            
+            if clockPreference == .c12 { dateFormatter.dateFormat = usd } else {dateFormatter.dateFormat = eud }
+            return dateFormatter.string(from: theDate!)
+        }
+        
+        set {
+            
+            if clockPreference == .c12 { dateFormatter.dateFormat = usd } else { dateFormatter.dateFormat = eud }
+            theDate = dateFormatter.date(from: newValue)
+        }
+    }
+    
+    var timeString: String {
+   
+        get {
+            
+            if clockPreference == .c12 { dateFormatter.dateFormat = ust } else {dateFormatter.dateFormat = eut }
+            return dateFormatter.string(from: theDate!)
+        }
+    }
+    
+    var dateAndTimeString: String {
+   
+        get {
+            
+            if clockPreference == .c12 {
+                
+                dateFormatter.dateFormat = usddt }
+            
+            else {dateFormatter.dateFormat = euddt }
+            
+            return dateFormatter.string(from: theDate!)
+        }
+    }
+    
+    // MARK: - INITIALIZATION
+    init (date: Date? = Date()) {
+        
+        super.init()
+
+        theDate = date
+        dateFormatter.amSymbol = "AM"
+        dateFormatter.pmSymbol = "PM"
+    }
+    
+    init (date: DateManager) {
+        
+        super.init()
+
+        theDate = date.localDate
+        dateFormatter.amSymbol = "AM"
+        dateFormatter.pmSymbol = "PM"
+    }
+    
+    init (fromServerDateAndTime: String) {
+        
+        super.init()
+        
+        serverDateAndTime = fromServerDateAndTime
+        dateFormatter.amSymbol = "AM"
+        dateFormatter.pmSymbol = "PM"
+    }
+    
+    init (fromDayDateAndTime: String) {
+        
+        super.init()
+        
+        dayDateAndTimeString = fromDayDateAndTime
+        dateFormatter.amSymbol = "AM"
+        dateFormatter.pmSymbol = "PM"
+    }
+    
+    init (fromGMTDayDateAndTime: String) {
+        
+        super.init()
+        
+        convertFromGMT(ddtGMT: fromGMTDayDateAndTime)
+       
+        dateFormatter.amSymbol = "AM"
+        dateFormatter.pmSymbol = "PM"
+    }
+    
+    init (fromDayAndDate: String) {
+        
+        super.init()
+     
+        dayAndDateString = fromDayAndDate
+        dateFormatter.amSymbol = "AM"
+        dateFormatter.pmSymbol = "PM"
+    }
+    
+    init (fromDate: String) {
+        
+        super.init()
+        
+        dateString = fromDate
+        dateFormatter.amSymbol = "AM"
+        dateFormatter.pmSymbol = "PM"
+    }
+       
+    // MARK: - METHODS
+    func gmtString(_ inFormat: DateString) -> String {
+        
+        let theGMTDate = DateManager(date: gmtDate)
+        
+        switch inFormat {
+            
+            case .date: return theGMTDate.dateString
+            case .dayAndDate: return theGMTDate.dayAndDateString
+            case .dayDateAndTime: return theGMTDate.dayDateAndTimeString
+        }
+    }
+    
+    func convertFromGMT(ddtGMT: String) {
+        
+        dayDateAndTimeString = ddtGMT
+        
+        var theLocalDate = Date()
+        let secondsFromGMT = TimeZone.current.secondsFromGMT()
+       
+        theLocalDate = theDate!.addingTimeInterval(TimeInterval(secondsFromGMT))
+        theDate = theLocalDate
+    }
+   
+    func todayIsInRangeTo(endDate: DateManager) -> Bool {
+        
+        let today = DateManager().dateComponents
+        let start = dateComponents
+        let end = endDate.dateComponents
+     
+        if today.year! >= start.year! && today.month! >= start.month! && today.day! >= start.day! {
+            
+            if today.year! <= end.year! && today.month! <= end.month! && today.day! <= end.day! { return true }
+        }
+        
+        return false
+    }
+    
+    func updateDateTo(newDate: DateManager) {
+        
+        dateComponents.year = newDate.dateComponents.year!
+        dateComponents.month = newDate.dateComponents.month!
+        dateComponents.day = newDate.dateComponents.day!
+        dateComponents.weekday = newDate.dateComponents.weekday!
+        
+        theDate = NSCalendar.current.date(from: dateComponents)!
+    }
+    
+    func updateTimeTo(newTime: DateManager) {
+        
+        dateComponents.hour = newTime.dateComponents.hour!
+        dateComponents.minute = newTime.dateComponents.minute!
+        
+        theDate = NSCalendar.current.date(from: dateComponents)!
     }
 }
+
+
+
--- Tests/DateManagerTests/DateManagerTests.swift
+++ Tests/DateManagerTests/DateManagerTests.swift
@@ -6,6 +6,6 @@ final class DateManagerTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(DateManager().text, "Hello, World!")
+        //XCTAssertEqual(DateManager().text, "Hello, World!")
     }
 }
